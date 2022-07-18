45a2150aad9a23ef3fa04b8cb4881784d57d1250
maqsoodahmad-1 <maqsoodmaxood75@gmail.com>
2022-07-18 23:21:21 +0530

    Changed the object having arrays in it to the array of objects where objects have arrays  in it

--- programming/chapter1/javascript/object.js
+++ programming/chapter1/javascript/object.js
@@ -1,13 +1,29 @@
-const person = {
-   name : ['Bob','smith'] ,
-   age : 32,
-   bio : function() {
-       console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
-     },
+let arr = [
+      {
+      name : ['Bob','smith'] ,
+      age : 32,
+      bio : function() {
+                console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+           },
+      introduceSelf : function() {
+             console.log(`Hi I'M ${this.name[0]}.`);
+           }, 
+        },
+     {
+    name : ['MAqsood', 'Ahmad'],
+    age : 33,
+    bio : function () {
+         console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`)
+        },
    introduceSelf : function() {
-       console.log(`Hi I'M ${this.name[0]}.`);
-       },
-   };
+         console.log(`Hi I'M ${this.name[1]}.`);
+     }
+     }
+   ]
 
-console.log(person.bio());
-console.log(person.name[0]);
+
+console.log(arr[0].bio());
+console.log(arr[0].introduceSelf());
+console.log(arr[1].bio());
+console.log(arr[1].introduceSelf());
+           
